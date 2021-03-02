92558d4e9457007c1e71e8bbdc1e7787761e1032
maxkuzmenchuk <maxkuzmenchuk@gmail.com>
2021-03-02 16:54:09 +0300

    get publication

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,6 +1,18 @@
 <template>
   <div>
     <p>CATALOG</p>
+
+    {{ publication.id }}
+    {{ publication.name }}
+    {{ publication.price }}
+    {{ publication.imageName }}
+    {{ publication.description }}
+
+  <div v-if="publication.cover != null">
+    <img v-bind:src="'data:image/jpeg;base64,' + publication.cover" />
+  </div>
+
+
   </div>
 </template>
 
@@ -14,8 +26,34 @@ import VueAxios from 'vue-axios';
 
 Vue.use(VueAxios, axios)
 
+const BASE_URL = 'http://localhost:9090';
+const instance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 1000,
+  headers: {
+    "Authorization": 'Bearer ' + localStorage.getItem('token')
+  }
+});
+
+
 export default {
-  name: "Catalog"
+  name: "Catalog",
+  data: () => ({
+    publication: {
+      id: null,
+      name: null,
+      price: null,
+      imageName: null,
+      description: true,
+      cover: null
+    }
+  }),
+  mounted() {
+    instance.get('/publication/13')
+        .then((resp) => {
+          this.publication = resp.data;
+        });
+  }
 }
 </script>
 
