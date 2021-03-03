c3dba8f485c58cf92e85b3af8ab3d5eeeca13a58
maxkuzmenchuk <maxkuzmenchuk@gmail.com>
2021-03-03 14:36:46 +0300

    catalog

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -1,18 +1,52 @@
 <template>
   <div>
-    <p>CATALOG</p>
+    <div class="elevation">
+      <div v-for="item of list" v-bind:key="item.id" class="md-elevation-24 viewport">
 
-    {{ publication.id }}
-    {{ publication.name }}
-    {{ publication.price }}
-    {{ publication.imageName }}
-    {{ publication.description }}
+        <md-toolbar :md-elevation="1" style="height: max-content">
+          <span class="md-title">{{ item.name }}
+          <md-tooltip md-delay="300" md-direction="top">{{ item.name }}</md-tooltip>
+            </span>
+        </md-toolbar>
 
-  <div v-if="publication.cover != null">
-    <img v-bind:src="'data:image/jpeg;base64,' + publication.cover" />
-  </div>
+        <md-list class="md-double-line">
+          <md-list-item>
+            <md-card-media>
+              <md-ripple>
+                <img v-bind:src="'data:image/jpeg;base64,' + item.cover" alt="cover">
+              </md-ripple>
+            </md-card-media>
+          </md-list-item>
 
+          <md-card-expand>
+              <md-card-expand-content>
+                <md-card-content>
+                  <p> {{ item.description }} </p>
+                </md-card-content>
+              </md-card-expand-content>
+          </md-card-expand>
 
+          <md-list-item>
+            <span> {{ item.price }}$</span>
+          </md-list-item>
+
+        </md-list>
+
+        <md-card-actions>
+          <md-button class="md-icon-button">
+            <md-icon>favorite</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>add_shopping_cart</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>share</md-icon>
+          </md-button>
+        </md-card-actions>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -39,24 +73,49 @@ const instance = axios.create({
 export default {
   name: "Catalog",
   data: () => ({
-    publication: {
-      id: null,
-      name: null,
-      price: null,
-      imageName: null,
-      description: true,
-      cover: null
-    }
+    list: null
   }),
   mounted() {
-    instance.get('/publication/13')
+    instance.get('/publication/all')
         .then((resp) => {
-          this.publication = resp.data;
+          this.list = resp.data;
+
+          console.log(this.list);
         });
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
+.elevation {
+  padding: 16px;
+  display: flex;
+  flex-wrap: wrap;
+
+}
+
+.viewport {
+  width: 300px;
+  max-width: 100%;
+  margin: 24px;
+  display: inline-block;
+  vertical-align: top;
+  align-items: center;
+  height: fit-content;
+  justify-content: center;
+  border: 1px solid rgba(#000, .12);
+}
+
+span {
+  min-width: 60px;
+  display: inline-block;
+  text-align: center;
+}
+
+p {
+  width: 100%;
+  white-space: normal;
+  word-wrap: break-word;
+}
 </style>
